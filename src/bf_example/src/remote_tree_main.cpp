1df4145c228dfe74260f7a009cd398cadec6fbba
rperez <rodrigo.perez@urjc.es>
2023-04-26 09:40:34 +0200

    Two main functions in th BF example

---
+++ src/bf_example/src/remote_tree_main.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <memory>
+
+
+#include "ament_index_cpp/get_package_share_directory.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "RemoteDelegateActionNode.hpp"
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+
+  // BF::RemoteDelegateActionNode node();
+  // node.setID("example");
+
+  // auto node = std::make_shared<BF::RemoteDelegateActionNode>(); //("example");
+  
+
+  rclcpp::shutdown();
+  return 0;
+
+}
