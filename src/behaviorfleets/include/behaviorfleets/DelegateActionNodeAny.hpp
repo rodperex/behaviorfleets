fe3cbe6b58dcb5557f3133d62dc399d5f0d8458b
rperez <rodrigo.perez@urjc.es>
2023-05-10 16:02:04 +0200

    Structure for any node executing BT

---
+++ src/behaviorfleets/include/behaviorfleets/DelegateActionNodeAny.hpp
@@ -0,0 +1,69 @@
+#ifndef BF__DELEGATEACTIONNODE_HPP_
+#define BF__DELEGATEACTIONNNODE_HPP_
+
+#include <string>
+
+#include "behaviortree_cpp/behavior_tree.h"
+#include "behaviortree_cpp/bt_factory.h"
+#include "behaviortree_cpp/decorator_node.h"
+
+#include "bf_msgs/msg/mission_command.hpp"
+#include "bf_msgs/msg/mission_status.hpp"
+
+
+#include "rclcpp/rclcpp.hpp"
+
+namespace BF
+{
+
+class DelegateActionNode : public BT::ActionNodeBase
+{
+public:
+
+  DelegateActionNode(
+    const std::string& name,
+    const BT::NodeConfig& conf);
+
+
+  void remote_status_callback(bf_msgs::msg::MissionStatus::UniquePtr msg);
+
+  
+  void halt() override
+  {}
+
+  static BT::PortsList providedPorts()
+  {
+    return {
+      BT::InputPort<std::string>("remote_tree"),
+      BT::InputPort<char*>("remote_id"),
+    };
+  }
+
+private:
+
+  static constexpr const char* MISSION = "";
+
+  rclcpp::Node::SharedPtr node_;
+  rclcpp::Publisher<bf_msgs::msg::MissionCommand>::SharedPtr tree_pub_;
+  rclcpp::Subscription<bf_msgs::msg::MissionStatus>::SharedPtr remote_sub_;
+  
+  bf_msgs::msg::MissionStatus::UniquePtr remote_status_;
+  std::string remote_id_, remote_tree_;
+  bool remote_itentified_ = false;
+
+  static const int FAILURE = 0;
+  static const int SUCCESS = 1;
+  static const int RUNNING = 2;
+
+  bool read_tree_from_port_;
+
+  
+
+  virtual BT::NodeStatus tick() override;
+  
+
+};
+
+}   // namespace BF
+
+#endif // BF__DELEGATE_HPP_
---
+++ src/behaviorfleets/src/behaviorfleets/DelegateActionNodeAny.cpp
@@ -0,0 +1,120 @@
+#include <string>
+#include <iostream>
+
+
+#include "behaviorfleets/DelegateActionNode.hpp"
+
+#include "behaviortree_cpp/behavior_tree.h"
+#include "bf_msgs/msg/mission_status.hpp"
+#include "behaviortree_cpp/bt_factory.h"
+#include "ament_index_cpp/get_package_share_directory.hpp"
+
+#include "behaviortree_cpp/utils/shared_library.h"
+#include "rclcpp/rclcpp.hpp"
+
+#include "bf_msgs/msg/mission_command.hpp"
+#include "bf_msgs/msg/mission_status.hpp"
+#include <fstream>
+
+namespace BF
+{
+
+DelegateActionNode::DelegateActionNode(
+  const std::string & xml_tag_name,
+  const BT::NodeConfig & conf)
+: BT::ActionNodeBase(xml_tag_name, conf)
+{
+  std::string pkgpath, xml_path;
+  
+  remote_id_ = "not_set";
+  remote_tree_ = "not_set";
+  config().blackboard->get("node", node_);
+  // config().blackboard->get("remote_tree", remote_tree_);
+  // config().blackboard->get("remote_id", remote_id_);
+  config().blackboard->get("pkgpath", pkgpath);
+  
+  getInput("mission_id", remote_id_);
+  getInput("remote_tree", remote_tree_);
+
+  std::cout << "remote_id: " << remote_id_ << std::endl;
+  std::cout << "remote_tree: " << remote_tree_ << std::endl;
+  
+  xml_path = pkgpath + remote_tree_;
+  std::cout << "xml_path: " << xml_path << std::endl;
+  std::ifstream file(xml_path);
+  std::ostringstream contents_stream;
+  contents_stream << file.rdbuf();
+  remote_tree_ = contents_stream.str();
+  
+  tree_pub_ = node_->create_publisher<bf_msgs::msg::MissionCommand>(
+    "/mission_command", 100);
+
+  remote_sub_ = node_->create_subscription<bf_msgs::msg::MissionStatus>(
+    "/" + remote_id_ + "/mission_status", rclcpp::SensorDataQoS(),
+    std::bind(&DelegateActionNode::remote_status_callback, this, std::placeholders::_1));  
+
+  
+  // PUBLISH TREE HERE
+  // std::cout << "TREE TO BE PUBLISHED TO " << remote_id_ << std::endl;
+  
+}
+
+void
+DelegateActionNode::remote_status_callback(bf_msgs::msg::MissionStatus::UniquePtr msg)
+{
+  remote_status_ = std::move(msg);
+  remote_itentified_ = true;
+
+  // if(!remote_itentified_){
+  //   remote_id = msg->robot_id;
+  //   remote_itentified_ = true;
+
+  //   remote_sub_ = node_->create_subscription<bf_msgs::msg::MissionStatus>(
+  //   "/" + remote_id + "/mission_status", rclcpp::SensorDataQoS(),
+  //   std::bind(&DelegateActionNode::remote_status_callback, this, std::placeholders::_1));
+
+  // }
+
+  // std::cout << "remote status callback" << std::endl;
+
+}
+
+BT::NodeStatus
+DelegateActionNode::tick()
+{ 
+  
+  if(!remote_itentified_){
+    bf_msgs::msg::MissionCommand msg;
+    msg.mission_tree = remote_tree_;
+    msg.robot_id = remote_id_;
+    tree_pub_->publish(msg);
+    std::cout << "tree publised in /" << remote_id_ << "/mission_command" << std::endl;
+  }
+
+  // int status = remote_status_->status;
+
+  // switch(status){
+  //   case RUNNING:
+  //     return BT::NodeStatus::RUNNING;
+  //     break;
+  //   case SUCCESS:
+  //     return BT::NodeStatus::SUCCESS;
+  //     break;
+  //   case FAILURE:
+  //     return BT::NodeStatus::FAILURE;
+  //     break;
+  //   }
+
+  //   return BT::NodeStatus::FAILURE;
+
+  // std::cout << "DelegateActionNode::tick()" << std::endl;
+  return BT::NodeStatus::RUNNING;
+}
+
+}  // namespace BF
+
+#include "behaviortree_cpp/bt_factory.h"
+BT_REGISTER_NODES(factory)
+{
+  factory.registerNodeType<BF::DelegateActionNode>("DelegateActionNode");
+}
