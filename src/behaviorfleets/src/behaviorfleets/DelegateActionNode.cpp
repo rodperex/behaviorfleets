61acfb7cc1311e46f80fe01ce7de97c7d6b0aa24
rperez <rodrigo.perez@urjc.es>
2023-05-31 18:38:07 +0200

    minor

--- src/behaviorfleets/src/behaviorfleets/DelegateActionNode.cpp
+++ src/behaviorfleets/src/behaviorfleets/DelegateActionNode.cpp
@@ -140,11 +140,14 @@ DelegateActionNode::mission_poll_callback(bf_msgs::msg::Mission::UniquePtr msg)
       RCLCPP_INFO(
         node_->get_logger(), (std::string("remote excluded: ") +
         "[ " + msg->robot_id + " ]").c_str());
-        // TO DO: publish a negative answer
-        // bf_msgs::msg::Mission mission_msg;
-        // mission_msg.msg_type = bf_msgs::msg::Mission::REJECT;
-        // mission_msg.robot_id = msg->robot_id;
-        // mission_msg.mission_id = msg->mission_id;
+        // publish a negative answer
+        bf_msgs::msg::Mission reject_msg;
+        reject_msg.msg_type = bf_msgs::msg::Mission::REJECT;
+        reject_msg.robot_id = msg->robot_id;
+        reject_msg.mission_id = msg->mission_id;
+        reject_msg.mission_id = mission_id_;
+        mission_pub_->publish(reject_msg);
+        return;
     }
     poll_answ_ = std::move(msg);
     remote_id_ = poll_answ_->robot_id;
